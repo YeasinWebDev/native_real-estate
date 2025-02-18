01392d059c416008fa87da2cc571b9bb9546e550
Yeasin <ar9485727@gmail.com>
2025-02-18 20:37:06 +0600

    solve nesting issue

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -24,18 +24,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { user } = useGlobalContext();
-  const parames = useLocalSearchParams<{ query?: string; filter?: string }>()
+  const parames = useLocalSearchParams<{ query?: string; filter?: string }>();
 
-  const { data: latestProperties, loading: latestPropertiesLoading } =
-    useAppwrite({
-      fn: getLatestProperties,
-    });
+  const { data: latestProperties, loading: latestPropertiesLoading } = useAppwrite({
+    fn: getLatestProperties,
+  });
 
-  const {
-    data: properties,
-    loading,
-    refetch,
-  } = useAppwrite({
+  const { data: properties, loading, refetch } = useAppwrite({
     fn: getProperties,
     params: {
       filter: parames.filter!,
@@ -55,196 +50,85 @@ export default function Index() {
 
   const handleCardPress = (id: string) => router.push(`/properties/${id}`);
 
-
   return (
-    <ScrollView>
-      <SafeAreaView style={{ backgroundColor: "#fff", height: "100%" }}>
-        <View style={styles.header}>
-          <View style={styles.headerLeft}>
-            <Image style={styles.headerAvater} source={{ uri: user?.avater }} />
-            <View>
-              <Text
-                style={{
-                  color: "#8C8E98",
-                  fontFamily: "Rubik-Regular",
-                  fontSize: 12,
-                }}
-              >
-                Good Morning
-              </Text>
-
-              <Text
-                style={{
-                  color: "#191D31",
-                  fontSize: 16,
-                  fontFamily: "Rubik-Medium",
-                }}
-              >
-                {user?.name}
-              </Text>
-            </View>
-          </View>
-          <View style={{ position: "relative" }}>
-            <Image style={styles.headerIcon} source={icons.bell} />
-            <View
-              style={{
-                width: 10,
-                height: 10,
-                borderRadius: 50,
-                backgroundColor: "#0061FF",
-                position: "absolute",
-                top: 0,
-                right: 3,
-                zIndex: 10,
-              }}
-            ></View>
-          </View>
-        </View>
-
-        <Search />
-
-        {/* Feature Section */}
-        <View style={{ marginBlock: 20, marginInline: 20 }}>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                fontFamily: "Rubik-SemiBold",
-                lineHeight: 24,
-              }}
-            >
-              Featured
-            </Text>
-            <TouchableOpacity>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: "Rubik-Bold",
-                  color: "#0061FF",
-                  lineHeight: 22.4,
-                }}
-              >
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <FlatList
-            data={latestProperties}
-            renderItem={({ item }) => (
-              <FeaturedCards
-                item={item}
-                onPress={() => handleCardPress(item?.$id)}
-              />
-            )}
-            keyExtractor={(item) => item.$id}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            ListEmptyComponent={
-              latestPropertiesLoading ? (
-                <ActivityIndicator
-                  size="large"
-                  style={{
-                    marginTop: 5,
-                  }}
-                />
-              ) : (
-                <NoResult />
-              )
-            }
-          />
-        </View>
-
-        {/* Recommendation */}
-        <View style={{ marginInline: 20, marginTop: 25 }}>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                fontFamily: "Rubik-SemiBold",
-                lineHeight: 24,
-              }}
-            >
-              Our Recommendation
-            </Text>
-            <TouchableOpacity>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: "Rubik-Bold",
-                  color: "#0061FF",
-                  lineHeight: 22.4,
-                }}
-              >
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <Filter/>
-
-        <View style={{ marginHorizontal: 20 }}>
-          <FlatList
-            data={properties}
-            numColumns={2}
-            keyExtractor={(_, index) => index.toString()}
-            renderItem={({ item }) => (
-              <View style={{ margin: 12 }}>
-                <Cards item={item} />
+    <SafeAreaView style={{ backgroundColor: "#fff", flex: 1 }}>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            {/* Header */}
+            <View style={styles.header}>
+              <View style={styles.headerLeft}>
+                <Image style={styles.headerAvater} source={{ uri: user?.avater }} />
+                <View>
+                  <Text style={styles.greeting}>Good Morning</Text>
+                  <Text style={styles.username}>{user?.name}</Text>
+                </View>
               </View>
-            )}
-            ListEmptyComponent={
-              loading ? (
-                <ActivityIndicator
-                  size="large"
-                  style={{
-                    marginTop: 5,
-                  }}
-                />
-              ) : (
-                <NoResult />
-              )
-            }
-          />
-        </View>
-      </SafeAreaView>
-    </ScrollView>
+              <View style={{ position: "relative" }}>
+                <Image style={styles.headerIcon} source={icons.bell} />
+                <View style={styles.notificationDot}></View>
+              </View>
+            </View>
+
+            <Search />
+
+            {/* Feature Section */}
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionTitle}>Featured</Text>
+              <TouchableOpacity>
+                <Text style={styles.seeAll}>See All</Text>
+              </TouchableOpacity>
+            </View>
+
+            <FlatList
+              data={latestProperties}
+              renderItem={({ item }) => (
+                <FeaturedCards item={item} onPress={() => handleCardPress(item?.$id)} />
+              )}
+              keyExtractor={(item) => item.$id}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              ListEmptyComponent={
+                latestPropertiesLoading ? <ActivityIndicator size="large" style={{ marginTop: 5 }} /> : <NoResult />
+              }
+            />
+
+            {/* Recommendation Section */}
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionTitle}>Our Recommendation</Text>
+              <TouchableOpacity>
+                <Text style={styles.seeAll}>See All</Text>
+              </TouchableOpacity>
+            </View>
+
+            <Filter />
+          </>
+        }
+        data={properties}
+        numColumns={2}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={{ margin: 12 }}>
+            <Cards item={item} />
+          </View>
+        )}
+        ListEmptyComponent={
+          loading ? <ActivityIndicator size="large" style={{ marginTop: 5 }} /> : <NoResult />
+        }
+      />
+    </SafeAreaView>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   header: {
-    display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingInline: 25,
+    paddingHorizontal: 25,
     marginTop: 10,
   },
   headerLeft: {
-    display: "flex",
     flexDirection: "row",
     alignItems: "center",
   },
@@ -259,5 +143,42 @@ const styles = StyleSheet.create({
     width: 26,
     height: 26,
   },
-  
+  notificationDot: {
+    width: 10,
+    height: 10,
+    borderRadius: 50,
+    backgroundColor: "#0061FF",
+    position: "absolute",
+    top: 0,
+    right: 3,
+    zIndex: 10,
+  },
+  greeting: {
+    color: "#8C8E98",
+    fontFamily: "Rubik-Regular",
+    fontSize: 12,
+  },
+  username: {
+    color: "#191D31",
+    fontSize: 16,
+    fontFamily: "Rubik-Medium",
+  },
+  sectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    marginTop: 25,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontFamily: "Rubik-SemiBold",
+    lineHeight: 24,
+  },
+  seeAll: {
+    fontSize: 16,
+    fontFamily: "Rubik-Bold",
+    color: "#0061FF",
+    lineHeight: 22.4,
+  },
 });
+
